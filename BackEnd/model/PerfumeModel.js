3f2d767dbef3eafb0a97a8c40ea55d82e1f6868b
Angel Oziel Carrillo Diaz de Leon <al500062@edu.uaa.mx>
2026-04-10 22:48:12 -0600

    Agregado el post, put y delete

--- BackEnd/model/PerfumeModel.js
+++ BackEnd/model/PerfumeModel.js
@@ -10,6 +10,30 @@ async function getAllPerfumes() {
     }
 }
 
+async function insertPerfume(codigo, nombre, precio, cantidad, marca, proveedor, tipo, linea) {
+    const [result] = await pool.query(
+        'INSERT INTO perfumes (Codigo, Nombre, Precio, Cantidad, Marca, Proveedor, Tipo, Linea) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+        [codigo, nombre, precio, cantidad, marca, proveedor, tipo, linea]
+    );
+    return result.affectedRows;
+}
+
+async function updatePerfume(codigo, nombre, precio, cantidad, marca, proveedor, tipo, linea) {
+    const [result] = await pool.query(
+        'UPDATE perfumes SET Nombre = ?, Precio = ?, Cantidad = ?, Marca = ?, Proveedor = ?, Tipo = ?, Linea = ? WHERE Codigo = ?',
+        [nombre, precio, cantidad, marca, proveedor, tipo, linea, codigo]
+    );
+    return result.affectedRows;
+}
+
+async function deletePerfume(codigo) {
+    const [result] = await pool.query('DELETE FROM perfumes WHERE codigo = ?', [codigo]);
+    return result.affectedRows;
+}
+
 module.exports = {
-    getAllPerfumes
+    getAllPerfumes,
+    insertPerfume,
+    updatePerfume,
+    deletePerfume
 }
--- BackEnd/routes/perfume.routes.js
+++ BackEnd/routes/perfume.routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { getPerfumes } = require("../Controllers/Perfume.controller.js");
+const { getPerfumes, postPerfumes, updatePerfume, deletePerfume } = require("../Controllers/Perfume.controller.js");
 
-router.get("/", getPerfumes);
+router.get("/getPerfumes", getPerfumes);
+router.post('/insertPerfume', postPerfumes);
+router.put('/actualizarPerfume/:codigo', updatePerfume);
+router.delete('/borrarPerfume/:codigo', deletePerfume);
 
 module.exports = router;

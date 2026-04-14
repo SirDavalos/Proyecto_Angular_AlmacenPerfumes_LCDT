4b1394744095f24a9f0477101ae98655dc03c889
AdrianoE-Dgz <areliedgz5005@gmail.com>
2026-04-13 21:43:40 -0600

    Avance Eliminar (falta signals)

--- FrontEnd/src/app/services/obtener-service.ts
+++ FrontEnd/src/app/services/obtener-service.ts
@@ -6,6 +6,7 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root',
 })
+
 export class ObtenerService {
   private http = inject(HttpClient);
   private url: string = 'http://localhost:3000/api/perfumes/getPerfumes';

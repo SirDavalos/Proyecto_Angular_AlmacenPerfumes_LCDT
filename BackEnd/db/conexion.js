79679a90540e0603f35d8d56253ed949825efd69
Davalos <fernandodava203@gmail.com>
2026-04-17 09:47:33 -0600

    Ajustes en la conexion de la BD

--- BackEnd/db/conexion.js
+++ BackEnd/db/conexion.js
@@ -1,11 +1,7 @@
 // db/conexion.js
 const mysql = require('mysql2/promise'); //1
 
-const pool = mysql.createPool( //2
-    procces.env.DB_URL
-);
-
-/*const pool = mysql.createPool({ //2
+const pool = mysql.createPool({ //2
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
@@ -15,5 +11,5 @@ const pool = mysql.createPool( //2
     connectionLimit: 10, //4
     queueLimit: 0, //5
     ssl: {rejectUnauthorized: false}
-});*/
+});
 module.exports = pool;

c17e031450c52375c22467215cd64497ca07ade6
AdrianoE-Dgz <areliedgz5005@gmail.com>
2026-05-31 08:24:54 -0600

    Feat: Agregar Proveedor Frontend

---
+++ FrontEnd/src/app/components/Proveedor/obtener/obtener.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-obtener',
+  imports: [],
+  templateUrl: './obtener.html',
+  styleUrl: './obtener.css',
+})
+export class Obtener {}
